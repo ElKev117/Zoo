0784c3a2e975278ad6471e11e44706f34cb6944b
ElKev117 <kevin.ruda@udea.edu.co>
2023-03-31 23:15:10 -0500

    pdf arreglado 2

---
+++ src/main/java/com/mycompany/zooproject/Controlador/Management.java
@@ -0,0 +1,23 @@
+package com.mycompany.zooproject.Controlador;
+import java.util.*;
+
+/**
+ * 
+ */
+public class Management {
+
+    /**
+     * Default constructor
+     */
+    public Management() {
+        
+    }
+
+    /**
+     * 
+     */
+    public void closeBoxOffice() {
+        // TODO implement here
+    }
+
+}
